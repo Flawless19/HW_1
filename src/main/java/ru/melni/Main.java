940c6396c0699d93d2df32bfd62a3f2d3f80c150
Alena Melnichenko <melnichenko2001@gmail.com>
2024-01-01 21:25:13 +0300

    Update Main.java

--- src/main/java/ru/melni/Main.java
+++ src/main/java/ru/melni/Main.java
@@ -2,6 +2,6 @@ package ru.melni;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello QA!");
+        System.out.println("Hello QA!!!!!");
     }
 }
